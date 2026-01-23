cc093683b1c2b6dc53f2d2dcdbdcc5b24db59852
temnijsvet <temnijsvet@gmail.com>
2026-01-23 16:07:47 +0300

    Теперь с историей!

--- kalc.cpp
+++ kalc.cpp
@@ -1,10 +1,18 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream> // Библиотека для работы с файлами
 using namespace std;
+
+
+
 int main() {
     setlocale(LC_ALL, "Russian");
     double num1, num2;
     char op;
-
+    ofstream MFile("history.txt"); MFile << "История:" << endl; MFile.close(); ofstream MyFile("history.txt", ios::app);// Заголовок истории
+    bool valid = true;
+    while (valid == true)
+    {
     cout << "Введите первое число: ";
     cin >> num1;
 
@@ -15,39 +23,42 @@ int main() {
     cin >> num2;
 
     double result;
-    bool valid = true;
+    
+        switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
 
-    switch (op) {
-    case '+':
-        result = num1 + num2;
-        break;
+        case '-':
+            result = num1 - num2;
+            break;
 
-    case '-':
-        result = num1 - num2;
-        break;
+        case '*':
+            result = num1 * num2;
+            break;
 
-    case '*':
-        result = num1 * num2;
-        break;
+        case '/':
+            if (num2 == 0) {
+                cout << "Ошибка: деление на ноль!" << endl;
+                valid = false;
+            }
+            else {
+                result = num1 / num2;
+            }
+            break;
 
-    case '/':
-        if (num2 == 0) {
-            cout << "Ошибка: деление на ноль!" << endl;
+        default:
+            cout << "Ошибка: неизвестный оператор!" << endl;
             valid = false;
         }
-        else {
-            result = num1 / num2;
+
+        if (valid) {
+            cout << "Результат: " << result << endl << endl;
+            MyFile << num1 << op << num2 << " = " << result << endl;
         }
-        break;
-
-    default:
-        cout << "Ошибка: неизвестный оператор!" << endl;
-        valid = false;
     }
 
-    if (valid) {
-        cout << "Результат: " << result << endl;
-    }
     int a; cin >> a;
+    MyFile.close();
     return 0;
 }
